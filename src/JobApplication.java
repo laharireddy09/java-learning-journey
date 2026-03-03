31e5b0b5e2284e8a34ab240facef30dcd3798d71
Laharireddy09 <laharireddy1112@gmail.com>
2026-03-02 19:44:03 -0600

    Day 2 - Variables and JobApplication class

---
+++ src/JobApplication.java
@@ -0,0 +1,19 @@
+public class JobApplication {
+
+    public static void main(String[] args){
+
+        //First Job application details
+
+        String companyName="Google";
+        String role = "Software Engineer";
+        double expectedSalary= 120000.0;
+        boolean isRemote = true;
+        int appliedYear = 2025;
+
+        System.out.println("Company: " + companyName);
+        System.out.println("Role: " + role);
+        System.out.println("Salary: " + expectedSalary);
+        System.out.println("Remote: " + isRemote);
+        System.out.println("Year: " + appliedYear);
+    }
+}
---
+++ src/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+
+    public static void main (String[] args){
+        String name="lahari";
+        int age=25;
+        double expectedSalary= 10000.5;
+        boolean isLookingForJob = true;
+        char experienceLevel = 'J';
+
+        System.out.println("Name: " +name);
+        System.out.println("Age: " +age);
+        System.out.println("Expected Salary: " + expectedSalary);
+        System.out.println("Looking for job: " + isLookingForJob);
+        System.out.println("Level: " + experienceLevel);
+    }
+}
