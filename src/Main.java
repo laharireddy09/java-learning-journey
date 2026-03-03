06ef1b6c6a29fedbf2a605bf127d5d30734e90de
lahari reddy <laharireddy@Mac.lan>
2026-03-02 19:14:03 -0600

    Day 1 - Hello world

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println("I am lahari and i am learning java!");
+        System.out.println("My Goal: AI/ML Engineer");
+    }
+}
